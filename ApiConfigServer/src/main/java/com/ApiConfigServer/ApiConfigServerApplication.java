c705bc1c0e84649c4c7b952969172b36cb443a31
praveen pal <pal.praveen11@gmail.com>
2023-06-26 23:32:03 +0100

    initial commit for config server

---
+++ ApiConfigServer/src/main/java/com/ApiConfigServer/ApiConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.ApiConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConfigServerApplication.class, args);
+	}
+
+}
